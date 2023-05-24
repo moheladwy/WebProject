a97c861afb452e6f8000a40530f241e21e4f8221
Shehab Diab <shehabgehaddiab@gmail.com>
2023-05-24 16:37:36 +0300

    separated a function

--- HumanResources/static/scripts/vacation-form-validation.js
+++ HumanResources/static/scripts/vacation-form-validation.js
@@ -2,23 +2,26 @@ import { isValidVacationDuration, markField } from "./helper-validations.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const submit = document.getElementById('submit-btn');
+    
+    submit.addEventListener('click', (e) => {
+        if (!isValidVacationForm()) {
+            e.preventDefault();
+        }
+    })
+})
+
+function isValidVacationForm()
+{
     const startDate = document.getElementById('start-date');
     const endDate = document.getElementById('end-date');
     
     const startDateError = document.getElementById('start-date-error')
     const endDateError = document.getElementById('end-date-error');
 
-    submit.addEventListener('click', (e) => {
-        let isValidVacationDates = isValidVacationDuration(startDate.value, endDate.value);
+    let isValidVacationDates = isValidVacationDuration(startDate.value, endDate.value);
 
-        let isValidForm = 
-            markField(() => isValidVacationDates, startDate, startDateError, "Dates Are Not Correct")
-        isValidForm = 
-            markField(() => isValidVacationDates, endDate, endDateError, "Dates Are Not Correct");
-
-        if (!isValidForm) {
-            e.preventDefault();
-        }
-    })
-})
-
+    let isValidForm = 
+        markField(() => isValidVacationDates, startDate, startDateError, "Dates Are Not Correct")
+    isValidForm = 
+        markField(() => isValidVacationDates, endDate, endDateError, "Dates Are Not Correct");
+}
