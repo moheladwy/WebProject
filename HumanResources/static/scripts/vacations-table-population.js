11a6003c0fd8e42e68af32000442c7b62060cd46
Kilany <y.m.elkilany@gmail.com>
2023-06-19 15:19:38 +0300

    Fix vacations bug

--- HumanResources/static/scripts/vacations-table-population.js
+++ HumanResources/static/scripts/vacations-table-population.js
@@ -38,24 +38,26 @@ function populateVacationsTable(vacations) {
             tbody.appendChild(tr);
         }
     }
+
+    console.log(vacations);
 }
 
 function createTableRow(vacation) {
     const tr = document.createElement('tr');
     tr.innerHTML = `
-    <td class="employee-id"></td>
-    <td class="employee-name"></td>
-    <td class="employee-email"></td>
-    <td class="employee-phone-number"></td>
-    <td class="vacation-start-date"></td>
-    <td class="vacation-end-date"></td>
-    <td class="vacation-reason"></td>
+    <td class="employee-id">${vacation.employee.id}</td>
+    <td class="employee-name">${vacation.employee.name}</td>
+    <td class="employee-email">${vacation.employee.email}</td>
+    <td class="employee-phone-number">${vacation.employee.phoneNumber}</td>
+    <td class="vacation-start-date">${vacation.startDate}</td>
+    <td class="vacation-end-date">${vacation.endDate}</td>
+    <td class="vacation-reason">${vacation.vacationReason}</td>
     <td>
         <button class="approve-button" type="button"><i class="fa-regular fa-circle-check"></i></button>
         <button class="reject-button" type="button"><i class="fa-regular fa-circle-xmark"></i></button>
     </td>
 `;
-    
+
     const approveButton = tr.querySelector('.approve-button');
     approveButton.addEventListener('click', () => handleVacationStatusChange(vacation.id, 'A'));
 
@@ -82,7 +84,7 @@ function handleVacationStatusChange(vacationId, status) {
     const tempForm = document.getElementById('temp-form');
     data = new FormData(tempForm);
     data.append('status', status);
-    
+
     putReq.send(data);
 }
 
